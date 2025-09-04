87730c44125237e01257ccd33ebd50b041266399
GalenYost <brawlstarser086@gmail.com>
2025-09-04 14:51:08 +0300

    c_cpp projects init

---
+++ c_cpp/flake.nix
@@ -0,0 +1,25 @@
+{
+   description = "gcc dev environment";
+
+   inputs = {
+      nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+   };
+
+   outputs = { self, nixpkgs }: let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+   in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+         buildInputs = [
+            pkgs.libgcc
+            pkgs.rocmPackages.clang
+         ];
+         shellHook = ''
+            export PS1='gcc-dev \W \$ '
+            cpp_run() {
+               local name="${1:-main}"
+               g++ "./$name.cpp" -o "./$name" && "./$name"
+            }
+         '';
+      };
+   };
+}
